a08d752d04ac55659b077510ffc97bc6ad481740
forThePing <546353955@qq.com>
2022-04-14 11:39:33 +0800

    添加环境全局变量设置

--- src/main/java/http/HttpTemplate.java
+++ src/main/java/http/HttpTemplate.java
@@ -2,11 +2,21 @@ package http;
 
 import template.Template;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class HttpTemplate<T> extends Template<T> {
 
+    private static Pattern pattern = Pattern.compile("^\\$.*\\$$");
     @Override
     protected String value(char[] chars, int offset) {
         String value = super.value(chars, offset);
-        return value;
+        Matcher matcher = pattern.matcher(value);
+        if (!matcher.find()) {
+            return value;
+        }
+        value = value.trim();
+        value = value.replace("$", "");
+        return EnvironmentConfig.getValue(value);
     }
 }
