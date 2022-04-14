4aaec0455803070d181d37bf47109b5095494f02
forThePing <546353955@qq.com>
2022-04-14 15:44:07 +0800

    环境变量由单引用改为复合引用

--- src/main/java/http/template/HttpEnvTemplate.java
+++ src/main/java/http/template/HttpEnvTemplate.java
@@ -3,21 +3,27 @@ package http.template;
 import http.EnvironmentConfig;
 import template.Template;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class HttpEnvTemplate<T> extends Template<T> {
 
-    private static Pattern pattern = Pattern.compile("^[$].*[$]$");
+    private static Pattern pattern = Pattern.compile("[$](.*?)[$]");
     @Override
     protected String value(char[] chars, int offset) {
         String value = super.value(chars, offset);
         Matcher matcher = pattern.matcher(value);
-        if (!matcher.find()) {
-            return value;
+        Map<String, String> cache = new HashMap<>();
+        while(matcher.find()) {
+            cache.put("\\$"+matcher.group(1)+"\\$", EnvironmentConfig.getValue(matcher.group(1)));
         }
         value = value.trim();
-        value = value.replace("$", "");
-        return EnvironmentConfig.getValue(value);
+        for (Map.Entry<String, String> entry : cache.entrySet()) {
+            value = value.replaceAll(entry.getKey(), entry.getValue());
+        }
+        return value;
     }
+
 }
